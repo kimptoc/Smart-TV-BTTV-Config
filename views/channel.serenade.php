997a14325107f5a519f3116cce1d8c5634949106
Chris Kimpton <chris@kimptoc.net>
2012-02-24 16:55:49 +0000

    basic html/view for channels page

--- views/channel.serenade.php
+++ views/channel.serenade.php
@@ -1,2 +1,54 @@
 <?php header('Access-Control-Allow-Origin: *') ?>
-div[event:click=handleShowStations] "foo"
+div
+  a#anchor[href="javascript:void(0);" event:click="Main.keyDown"]
+  div#main
+    div#logo "Bagel Tech Mac"
+      div#rightHalf
+          div#videoBox_top
+          div#videoBox_left
+          div#videoBox_right
+          div#videoBox_bottom
+              div#time
+                  div#progressBarBG
+                      div#progressBar
+                  div#timeInfo
+              div#volume
+                  div#volumeIcon
+                  div#volumeBarBG
+                      div#volumeBar
+                  div#volumeInfo
+              div#videoControl
+                  div#rewind
+                  div#play
+                  div#stop
+                  div#pause
+                  div#forward
+              div#status
+          div#description_top
+          div#description_bottom
+          div#description
+      div#leftHalf
+          div#videoList.style_videoList
+              div#video0
+              div#video1
+              div#video2
+              div#video3
+              div#video4
+              div#videoCount
+          div#previous
+          div#next
+      div#navi
+        div#help_navi
+          img[src="images/navi/play.png"]
+          img[src="images/navi/stop.png"]
+          img[src="images/navi/pause.png"]
+          img[src="images/navi/rewind.png"]
+          img[src="images/navi/forward.png"]
+          a.style_navi
+            "Video Control"
+          img[src="images/navi/enter.png"]
+          a.style_navi
+            "Full-screen/Window mode"
+          img[src="images/navi/return.png"]
+          a.style_navi
+            "Return"
