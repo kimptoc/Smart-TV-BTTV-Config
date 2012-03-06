bc228de216b09fab929ce9cf9aa9b0f0e0198139
Chris Kimpton <chris@kimptoc.net>
2012-03-06 21:44:05 +0000

    encode feed name, a little security

--- feed.php
+++ feed.php
@@ -1,2 +1,2 @@
 <?php header('Access-Control-Allow-Origin: *') ?>
-<?php echo file_get_contents("http://feeds.feedburner.com/" . $_GET["name"]); ?>
+<?php echo file_get_contents("http://feeds.feedburner.com/" . urlencode($_GET["name"])); ?>
